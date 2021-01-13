73db45019107e27cde07854126046b8248128382
Christopher Biessener <cpbiessener@gmail.com>
2021-01-13 09:49:42 -0600

    fix dev port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 // deployment port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 // initialize Express
 const app = express();
 
