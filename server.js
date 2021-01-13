c24a09cab2c11d8bd5a15e19aedddabc49c28b28
Christopher Biessener <cpbiessener@gmail.com>
2021-01-13 09:56:06 -0600

    async mongoose connect

--- server.js
+++ server.js
@@ -22,13 +22,14 @@ mongoose.Promise = Promise;
 // Connect to the Mongo DB
 // If deployed, use the deployed database. Otherwise use the local scraper database
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/testbed";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+let mongoInstance;
 
 // import routes and give the server access to them
 const routes = require("./routes");
 app.use(routes);
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, async function() {
+  mongoInstance = await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
