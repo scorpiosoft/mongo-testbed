51e968c81bab844ee2bc9740de131479cbbc6e02
Christopher Biessener <cpbiessener@gmail.com>
2021-01-18 10:28:40 -0600

    setAge requires existing data

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,46 +1,50 @@
 const Test = require("../models/test");
 const util = require("../util/util");
 
-// Defining methods for the testController
+// getAge - get the 'age' field
+// TODO - allow a 'title' or 'id' to be passed in
 function getAge(req, res) {
   console.log('testController:getAge');
   Test.findOne({}, 'age _id')
       .then(data => {res.json(data);console.log(data)})
       .catch(err => res.status(422).json(err));
+    // Test.findById(req.body._id)
+    //     .then(data => res.json(data))
+    //     .catch(err => res.status(422).json(err));
+    // data = { age: { value: 13, votes: [{user: req.body.u, value: req.body.a}], avg: req.body.a } };
 }
+// setAge - set the 'age' field
+//   requires the request body to be a complete 'age' document, also _id
 function setAge(req, res) {
-  console.log('testController:setAge');
-  let data = undefined;
-  if (util.validate_exists(req.body._id))
+  let data = req.body;
+  console.log(`testController:setAge:data(${JSON.stringify(data,null,2)})`);
+  if (util.validate_exists(data._id))
   {
-    Test.findById(req.body._id)
-        .then(data => res.json(data))
-        .catch(err => res.status(422).json(err));
-    console.log(`setAge:findById:data:${JSON.stringify(data,null,2)}`);
+    console.log('testController:setAge - data._id does not exist');
+    return undefined;
   }
-  console.log(`sanity check:data:${JSON.stringify(data,null,2)}`);
   // check if this user already voted
-  if (util.validate_exists(data))
+  if (util.validate_exists(data.age.votes))
   {
-    const votedIdx = data.age.votes.find(e => e.user === req.body.u);
+    const votedIdx = data.age.votes.find(e => e.user === data.u);
     if (votedIdx)
     {
-      data.age.votes[votedIdx].value = req.body.a;
+      data.age.votes[votedIdx].value = data.a;
     } else {
-      data.age.votes.push({user: req.body.u, value: req.body.a});
+      data.age.votes.push({user: data.u, value: req.body.a});
     }
     const len = data.age.votes.length;
     const tot = data.age.votes.reduce((accum, cur) => accum + cur.value, 0);
     data.age.avg = Math.round(tot/len);
   } else {
     // technically, should never get here
-    console.log(`id (${req.body._id}) not found`)
-    data = { age: { value: 13, votes: [{user: req.body.u, value: req.body.a}], avg: req.body.a } };
+    console.log(`testController:setAge no 'votes' property: (${JSON.stringify(data.age,null,2)})`)
+    return undefined;
   }
   // const options = {
   //   upsert: true
   // }
-  console.log(`updateOne: (${JSON.stringify(data,null,2)})`)
+  console.log(`testController:setAge updateOne: (${JSON.stringify(data,null,2)})`)
   // Test.updateOne({_id: data._id}, data, options)
   Test.updateOne({_id: data._id}, data)
       .then(data => res.json(data))
