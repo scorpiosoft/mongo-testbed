91d2f5cfdac69bda194c70f5ec9b572bdbe291ec
Christopher Biessener <cpbiessener@gmail.com>
2021-01-18 09:22:12 -0600

    add CORS stuff

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const PORT = process.env.PORT || 3001;
 // initialize Express
 const app = express();
 
-// get around CORS
+// get around CORS for development
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers");
+next();
 });
 
 //
